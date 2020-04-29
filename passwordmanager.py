c16aa32807ff1655f0a05eb87a63f932289ee0a1
Rakesh Sadu <rsadu@apple.com>
2020-04-29 10:41:05 -0700

    Updates with MasterPassword

--- passwordmanager.py
+++ passwordmanager.py
@@ -20,15 +20,16 @@ import logging
 import argparse
 import csv
 import pprint
-import collections
+import hashlib, binascii, os
 from six.moves import configparser
 from datetime import datetime
 
 ###################
 config = configparser.ConfigParser()
 script_location = os.path.dirname(os.path.realpath(__file__))
-config.read(script_location+'/passmanager.ini')
 
+configfile_path = script_location+'/passmanager.ini'
+config.read(configfile_path)
 path = config.get('DEFAULT','file_path')
 log_location = config.get('DEFAULT','logging_location')
 filename = config.get('DEFAULT','file_name')
@@ -56,7 +57,34 @@ if not os.path.exists(passwordfile):
         writer =csv.DictWriter(passfile, fieldnames=fields)
         #writting the header
         writer.writeheader()
-        print('A New Password file is created')
+        Master_password = str(input("Give A Master Password to secure the Credentials file :"))
+        salt = hashlib.sha256(os.urandom(60)).hexdigest().encode('ascii')
+        Mstrpwdhash = hashlib.pbkdf2_hmac('sha1', Master_password.encode('utf-8'), salt, 100000)
+        Mstrpwdhash = binascii.hexlify(Mstrpwdhash)
+        Stored_Master_Password = (salt + Mstrpwdhash).decode('ascii')
+        with open(configfile_path, 'w') as cfgfile:
+            config.set('DEFAULT','Master_Hash', Stored_Master_Password)
+            config.write(cfgfile)
+            print('A New Password file is created')
+
+def Verify_Password():
+    Stored_Pass = config.get('DEFAULT', 'Master_Hash')
+    salt = Stored_Pass[:64]
+    Stored_Pass = Stored_Pass[64:]
+    count = 0
+    while True:
+        Given_Password = input('Please provide your MasterPassword to Continue :')
+        Pwdhash = hashlib.pbkdf2_hmac('sha1', Given_Password.encode('utf-8'),salt.encode('ascii'), 100000)
+        pwdhash = binascii.hexlify(Pwdhash).decode('ascii')
+        count+=1
+        if pwdhash != Stored_Pass and count<3:
+            print ('Your Master Password is Incorrect, Please try again')
+            continue
+        elif pwdhash == Stored_Pass:
+            break
+        else:
+            print ('Incorrect password in all 3 attempts, Exiting now....!!!')
+            exit()
 
 print('#########################################')
 print("Add: To add new Credentials")
@@ -71,7 +99,7 @@ Actions = ['Add','Delete','Update','Get']
 #Making Sure the User only Gives the Valid inputs.
 while True:
     #Taking an input from the user
-    Action_perform = input('Please Enter the Action that you would like to perform :')
+    Action_perform = input('Enter the Action that you would like to perform :')
     if str(Action_perform) in Actions:
         break
 
@@ -79,6 +107,7 @@ def work_csv():
     #Adding a new Service Credentials
     fields = ['Service','Email','Username','Password','Phonenumber','LastUpdate_Date']
     if str(Action_perform)=='Add':
+        Verify_Password()
         Service_Name = input('Enter the Servicename: ')
         with open(passwordfile, 'r') as csvfile:
             reader = csv.DictReader(csvfile)
@@ -102,13 +131,13 @@ def work_csv():
 
     #Deleting a service Credentails from a file.
     elif str(Action_perform)=='Delete':
+        Verify_Password()
         Val = []
         service_delete=str(input('Enter the available servicename you want to delete :'))
         with open(passwordfile, 'r') as readFile:
             reader = csv.reader(readFile)
             reader = list(reader)
             for row in reader:
-                #print(row[0])
                 if not row[0]==service_delete:
                      Val.append(row)
         with open(passwordfile, 'w') as writeFile:
@@ -117,6 +146,7 @@ def work_csv():
 
     #Function to get the credentials
     elif str(Action_perform)=='Get':
+        Verify_Password()
         Read_Cred = str(input('Please Enter the Service to get credentials :'))
         with open(passwordfile, 'r') as readFile:
             reader = csv.reader(readFile)
@@ -135,6 +165,7 @@ def work_csv():
 
     #Function To Update the Credentials
     elif str(Action_perform)=='Update':
+        Verify_Password()
         Val = []
         To_be_written=dict()
         Update_service = str(input('Please enter the service to update :'))
